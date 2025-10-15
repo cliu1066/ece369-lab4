111e2e66bbda2bd4c451bc23d44b4ea7e1aa34e5
agharteyaz <167935447+agharteyaz@users.noreply.github.com>
2025-10-15 15:54:08 -0700

    Create Mux32Bit2To1.v

---
+++ Mux32Bit2To1.v
@@ -0,0 +1,22 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// ECE369 - Computer Architecture
+// 
+// Module - Mux32Bit2To1.v
+// Description - Performs signal multiplexing between 2 32-Bit words.
+////////////////////////////////////////////////////////////////////////////////
+
+module Mux32Bit2To1(out, inA, inB, sel);
+
+    output reg [31:0] out;
+    
+    input [31:0] inA;
+    input [31:0] inB;
+    input sel;
+
+    always @(*) begin
+        assign out = (sel == 1'b0) ? inA : inB;
+    end
+
+endmodule
