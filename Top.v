61585d78071b03c89c7ed793b84d417e98f34689
agharteyaz <167935447+agharteyaz@users.noreply.github.com>
2025-11-03 16:15:44 -0700

    Update Top.v

--- Top.v
+++ Top.v
@@ -40,30 +40,57 @@ module Top(Clk, Rst);
     
     // RegisterFile
     wire RegWrite;
-    wire [4:0] ReadRegister1, ReadRegister2;
     wire [4:0] MEM_WB_WriteRegister;
     wire [31:0] RegWriteData;
     wire [31:0] ReadData1, ReadData2;
-    RegisterFile m6(ReadRegister1, ReadRegister2, MEM_WB_WriteRegister, RegWriteData, RegWrite, Clk, ReadData1, ReadData2);
+    RegisterFile m6(IF_ID_Instruction_Out[25:21], IF_ID_Instruction_Out[20:16], MEM_WB_WriteRegister, RegWriteData, RegWrite, Clk, ReadData1, ReadData2);
     
     // Sign Extend
     wire [31:0] Imm_SE;
-    SignExtension m7(IF_ID_Instruction_Out, Imm_SE);
+    SignExtension m7(IF_ID_Instruction_Out[15:0], Imm_SE);
+
+    //control
+    wire RegDst, Jump, Branch, MemRead, MemToReg, MemWrite, ALUSrc;
+    wire [3:0] ALUOp;
+    Controller m8(IF_ID_Instruction_Out[31:26], IF_ID_Instruction_Out[5:0], RegDst, Jump, Branch, MemRead, MemToReg, ALUOp, MemWrite, ALUSrc, RegWrite, IF_ID_Instruction_Out[20:16]);
+
+    //ID/EX
+    wire ID_EX_RegDst, ID_EX_Jump, ID_EX_Branch, ID_EX_MemRead, ID_EX_MemToReg, ID_EX_MemWrite, ID_EX_ALUSrc, ID_EX_RegWrite;
+    wire [3:0] ID_EX_ALUOp;
+    wire [31:0] ID_EX_Jump_Addr;
+    wire [31:0] ID_EX_PC_AddResult;
+    wire [31:0] ID_EX_ReadData1, ID_EX_ReadData2;
+    wire [31:0] ID_EX_Imm_SE;
+    wire [4:0] ID_EX_Rs, ID_EX_Rt, ID_EX_Rd;
+    wire [5:0] ID_EX_Funct, ID_EX_OpCode;
+    ID_EX_Reg m9(Clk, Rst,
+    RegWrite, MemToReg,
+    Branch, MemRead, MemWrite, Jump,
+    RegDst, ALUSrc,
+    ALUOp,
     
-    // RegFile
-    wire [4:0] Rs, Rt, Rd, EX_Rs, EX_Rt, EX_Rd, EX_WriteReg, MEM_WriteReg, WB_WriteReg;
-    wire [31:0] WB_WriteData, ReadData1, ReadData2, EX_ReadData1, EX_ReadData2;
+    JumpAddress, IF_ID_PC_Out,
+    ReadData1, ReadData2, Imm_SE,
+    IF_ID_Instruction_Out[25:21], IF_ID_Instruction_Out[20:16], IF_ID_Instruction_Out[15:11],
+    IF_ID_Instruction_Out[5:0], IF_ID_Instruction_Out[31:26],
+                 
+    ID_EX_RegWrite, ID_EX_MemToRe,
+    ID_EX_Branch, ID_EX_MemRead, ID_EX_MemWrite, ID_EX_Jump,
+    ID_EX_RegDst, ID_EX_ALUSrc,
+    ID_EX_ALUOp,
+    ID_EX_Jump_Addr, ID_EX_PC_AddResult,
+    ID_EX_ReadData1, ID_EX_ReadData2, ID_EX_Imm_SE,
+    ID_EX_Rs, ID_EX_Rt, ID_EX_Rd,
+    ID_EX_Funct, ID_EX_OpCode
+    );
     
-    // ALU
-    wire [31:0] EX_ALUResult, MEM_ALUResult, WB_ALUResult;
-    wire ALUZero;
+    //RegDst Mux
+    wire [4:0] EX_RegDst_Out; 
+    //output to RegDst mux in EX stage of pipeline
+    Mux32Bit2To1(EX_RegDst_Out, ID_EX_Rt, ID_EX_Rd, ID_EX_RegDst);
+
+    //ALU
     
-    // Control Signals
-    wire RegDst, Jump, Branch, MemRead, MemToReg, MemWrite, ALUSrc, RegWrite;
-    wire ID_RegDst, ID_Jump, ID_Branch, ID_MemRead, ID_MemToReg, ID_MemWrite, ID_ALUSrc, ID_RegWrite;
-    wire EX_RegDst, EX_Jump, EX_Branch, EX_MemRead, EX_MemToReg, EX_MemWrite, EX_ALUSrc, EX_RegWrite;
-    wire MEM_MemToReg, MEM_RegWrite, MEM_MemRead, MEM_MemWrite;
-    wire WB_MemToReg, WB_RegWrite;
-    wire [3:0] ALUOp, ID_ALUOp, EX_ALUOp;
+    
     
 endmodule
